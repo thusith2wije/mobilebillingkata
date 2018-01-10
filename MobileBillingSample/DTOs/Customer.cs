109f718a5bdb6b48c65da978a6bfefb1d30f7be1
Niruka <niruka@outlook.aom>
2018-01-10 11:19:45 +0530

    Added empty stubs

---
+++ MobileBillingSample/DTOs/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MobileBillingSample.DTOs
+{
+    /// <summary>
+    /// Mobile customer details
+    /// </summary>
+    public class Customer
+    {
+        public string FullName { get; set; }
+        public string BillingAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string PackageCode { get; set; }
+        public DateTime RegisteredDate { get; set; }
+    }
+}
